8e3ed84fbcf991677ca227cddd542cb02ff44892
sttk <sttk.xslet@gmail.com>
2019-05-28 23:30:22 +0900

    Test: Fix that appveyor failed.

--- test/no-arg/no-arg.test.js
+++ test/no-arg/no-arg.test.js
@@ -4,6 +4,13 @@ var getColorDepth = require('../../');
 var tty = require('tty');
 var assert = require('assert');
 
-if (typeof tty.WriteStream.prototype.getColorDepth === 'function') {
+// tty.WriteStream.getColorDepth was implememented since nodejs v9.9.
+// But before v11, this function's behavior is slightly different from
+// the latest.
+// if (tty.WriteStream.prototype.getColorDepth) {
+var versions = /^v([0-9]+)\.([0-9]+)\.([0-9]+)/.exec(process.version)
+  .slice(1, 4)
+  .map(Number);
+if (versions[0] >= 11) {
   assert.equal(getColorDepth(), tty.WriteStream.prototype.getColorDepth());
 }
